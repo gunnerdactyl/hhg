1721df140fdfc8f9054c4663d205f9dd5e2fd2f8
gunnerdactyl <160198419+gunnerdactyl@users.noreply.github.com>
2024-02-28 21:10:55 -0500

    add markdown instructions

--- hhg.py
+++ hhg.py
@@ -4,6 +4,17 @@ from thefuzz import fuzz, process
 
 
 st.title("Happy Hunting Grounds")
+st.markdown(
+    """
+    Happy Hunting Grounds (HHG) is a game where players test their knowledge of Premier League away goals.
+
+    The game format was created by [Adam Hurrey](https://twitter.com/footballcliches) for the [Football Cliches podcast](https://linktr.ee/footballcliches).
+
+    This online version of HHG was created by me, Jacob. I am [@gunnerdactyl](https://twitter.com/gunnerdactyl), for my sins.
+
+    If you find any bugs, bad data, or weird behavior in the app, email gunnerdactyl [at] gmail.
+    """
+)
 
 if "player1_name" not in st.session_state:
     st.session_state["player1_name"] = "Player 1"
@@ -37,8 +48,53 @@ if "progress_buttons_disabled" not in st.session_state:
     st.session_state["progress_buttons_disabled"] = True
 
 
-with st.expander("Instructions"):
-    st.text("This is how to play.")
+
+with st.expander("Instructions and details"):
+    st.markdown(
+        """
+        ## Gameplay
+
+        The two players take turns. During their turn, the player selects a difficulty level with the slider (1=easiest, 10=hardest). This difficulty level also represents the number of points that the player can earn for a fully-correct answer.
+
+        Players are presented with a "Team @ Venue" hunting ground prompt and they must name a player who scored for the given team at that particular away ground and the season during which they did it. The game covers the Premier League seasons from 1992/1993 through 2022/2023. (2023/2024 goals are not included!)
+
+        ## Scoring
+
+        If the player correctly names a goalscorer for the given team at the given venue, they receive points equal to half the difficulty level. If they also get the season correct, they receive the other half of the points. No points are awarded for having the correct season but an incorrect name.
+
+        ## Difficulty levels
+
+        The difficulty level of a hunting ground is set using a combination of team performance and the number of away goals scored by a team at a particular venue. This reflects two guiding assumptions about difficulty:
+
+        1. Goals in games between the most successful teams are easier to remember.
+        2. Matchups where more away goals have been scored are easier to remember, though this factor is not as powerful as the first.
+
+        For each away goal scored during the Premier League era, the away _and_ home teams received a score based on their final league position during that particular season. Then, those scores for the two teams were added together and called the `team performance` for that goal.
+
+        Next, for each away team/home team/stadium combination, the `average team performance` of all the away goals scored was calculated.
+
+        Also for each combination, the total number of away goals was divided by two and called `goal volume`. Raw difficulty points for the matchup were calculated as `average team performance + goal volume`.
+
+        Armed with raw difficulty scores for all the away/home/stadium combinations, the Jenks natural breaks algorithm was applied to create ten difficulty thresholds.
+
+        Here is an example hunting ground for each difficulty level.
+
+        | Difficulty | Example hunting ground|
+        | :--------: | :---------- |
+        | 1 |  Tottenham Hotspur @ Emirates Stadium |
+        | 2 |  Newcastle United @ Goodison Park |
+        | 3 |  Southampton @ Anfield |
+        | 4 |  Stoke City @ Old Trafford |
+        | 5 |  Birmingham City @ Emirates Stadium |
+        | 6 |  Bournemouth @ Tottenham Hotspur Stadium |
+        | 7 |  Crystal Palace @ Stadium of Light |
+        | 8 |  Watford @ Cardiff City Stadium |
+        | 9 |  Brentford @ Turf Moor |
+        | 10 |  Ipswich Town @ Selhurst Park |
+
+
+        """            
+    )
 
 
 @st.cache_data
